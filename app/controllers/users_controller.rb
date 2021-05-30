2825610f9b3f7068195d7f60b2ee04e7a8a5cd0c
KeslieBox <kesliebox@gmail.com>
2021-05-30 10:25:21 -0500

    sets up user_params and create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
     def index
         render json: User.all, key_transform: :camel_lower
     end
@@ -7,5 +6,18 @@ class UsersController < ApplicationController
     def show 
         user = User.find_by(id: params[:id])
         render json: user, key_transform: :camel_lower
+    end
+
+    def create 
+        binding.pry
+        user = User.create(user_params)
+        render json: user, key_transform: :camel_lower
+    end
+
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:username, :password)
     end
 end
